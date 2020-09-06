e29a95c8d134f4e86873de41a639166f86d90203
shubham1507 <shubham.joshi1507@gmail.com>
2020-09-06 11:18:37 +0530

    HttpResponse

--- updates/views.py
+++ updates/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+import json
 
 # Create your views here.
 
@@ -8,4 +9,15 @@ def update_model_detail_view(request):
 
     data = {"count": 1000, "content": "adsf"}
 
-    return JsonResponse(data)
+    json_data = json.dumps(data)
+    '''
+        Django uses request and response objects to pass state through the system. Each view is responsible for returning an HttpResponse object. Using HttpResponse you need to first serialize your object to JSON.
+
+Whereas,
+
+Since version 1.7, Django counts with the built-in JsonResponse class, which is a subclass of HttpResponse. Its default Content-Type header is set to application/json, which is really convenient. It also comes with a JSON encoder, so you don’t need to serialize the data before returning the response object.
+    '''
+
+    # return JsonResponse(data)
+
+    return HttpResponse(json_data, content_type='application/json')
